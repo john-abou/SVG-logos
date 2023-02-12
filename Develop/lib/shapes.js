ed0bb474e7829889fb1e4485caf319a412e46d1f
John Abou <abounassar.john@gmail.com>
2023-02-12 18:45:21 -0500

    Added the Shape and square classes.

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -1,6 +1,6 @@
-
-class Shapes {
-    constructor(text = "", textColor="blue", shape="circls", shapeColor="lightgreen") {
+// Must have parent Class called shape -- common traits include text, text color, shape color, height, width
+class Shape {
+    constructor(text="SVG", textColor="white", shape="circle", shapeColor="black") {
         this.text = text;
         this.textColor = textColor;
         this.shape = shape;
@@ -8,11 +8,41 @@ class Shapes {
     }
 
     render() {
-        console.log("The child classes render method is not working.")
+        return `<svg xmlns="http://www.w3.org/2000/svg" height="200" width="300" style="text-align: center;">
+        <!-- rectangle -->
+        <${shape} style="fill:${shapeColor}" />
+        
+        <text x=50% y="50%" style="font-size: 32px;  text-align:center;" text-anchor="middle" alignment-baseline="middle" fill=${this.textColor}>
+        ${this.text}
+        </text>
+        </svg>`
     }
-
-    renderShape() {
-    }
-
-
 }
+
+class Square extends Shape {
+    constructor(text, textColor, shape, shapeColor) {
+        super(text);
+        super(textColor);
+        super(shape) = 'rect x="55" y="10" width="190" height="190"';
+        super(shapeColor);
+    }
+}
+
+
+html = `<svg xmlns="http://www.w3.org/2000/svg" height="200" width="300" style="text-align: center;">
+<!-- rectangle -->
+<ellipse cx="150" cy="100" rx="125" ry="75" style="fill:${shapeColor};stroke:purple;stroke-width:2" />
+
+<text x=50% y="50%" style="font-size: 32px;  text-align:center;" text-anchor="middle" alignment-baseline="middle" fill=${textColor}>
+${text}
+</text>
+</svg>`;
+
+
+/*
+    <rect x="25" y="25" width="270" height="170" fill="black"/> 
+    <rect x="55" y="10" width="190" height="190" fill="black"/> 
+    <circle cx="150" cy="100" r="90" fill="red" stroke="black" stroke-width="1" />
+    <polygon points="150,10 290,190 10,190" style="fill:lime;stroke:purple;stroke-width:1" />
+    <ellipse cx="150" cy="100" rx="125" ry="75" style="fill:yellow;stroke:purple;stroke-width:2" />
+*/
--- index.js
+++ index.js
@@ -21,3 +21,49 @@ WHEN I open the `logo.svg` file in a browser THEN I am shown a 300x200 pixel ima
 // From input instantiate a new class of the selected shape
 // Use classes methods to update text, text color, shape color --> these methods should be in Shape class (parent)
 // Use child class to have a specific method to make shape height, width, etc 
+
+// Import dependencies and Shape objects
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { shape, square,} = shapes.js;
+
+// Define global variables
+const questions = [
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to 3 Characters:'
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter a text color:'
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape would you like?',
+      choices: [
+        'Circle',
+        'Square',
+        'Rectangle',
+        'Triangle',
+        'Elipse'
+      ]
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter a shape color:'
+    }
+];
+const hexRegex = /^([a-fA-F0-9]{6}||[a-fA-F0-9]{3})$/;
+const colorKeywords = "AliceBlue,AntiqueWhite,Aqua,Aquamarine,Azure,Beige,Bisque,Black,BlanchedAlmond,Blue,BlueViolet,Brown,BurlyWood,CadetBlue,Chartreuse,Chocolate,Coral,CornflowerBlue,Cornsilk,Crimson,Cyan,DarkBlue,DarkCyan,DarkGoldenRod,DarkGrey,DarkGreen,DarkKhaki,DarkMagenta,DarkOliveGreen,Darkorange,DarkOrchid,DarkRed,DarkSalmon,DarkSeaGreen,DarkSlateBlue,DarkSlateGrey,DarkTurquoise,DarkViolet,DeepPink,DeepSkyBlue,DimGray,DodgerBlue,FireBrick,FloralWhite,ForestGreen,Fuchsia,Gainsboro,GhostWhite,Gold,GoldenRod,Grey,Green,GreenYellow,HoneyDew,HotPink,IndianRed,Indigo,Ivory,Khaki,Lavender,LavenderBlush,LawnGreen,LemonChiffon,LightBlue,LightCoral,LightCyan,LightGoldenRodYellow,LightGrey,LightGreen,LightPink,LightSalmon,LightSeaGreen,LightSkyBlue,LightSlateGrey,LightSteelBlue,LightYellow,Lime,LimeGreen,Linen,Magenta,Maroon,MediumAquaMarine,MediumBlue,MediumOrchid,MediumPurple,MediumSeaGreen,MediumSlateBlue,MediumSpringGreen,MediumTurquoise,MediumVioletRed,MidnightBlue,MintCream,MistyRose,Moccasin,NavajoWhite,Navy,OldLace,Olive,OliveDrab,Orange,OrangeRed,Orchid,PaleGoldenRod,PaleGreen,PaleTurquoise,PaleVioletRed,PapayaWhip,PeachPuff,Peru,Pink,Plum,PowderBlue,Purple,Red,RosyBrown,RoyalBlue,SaddleBrown,Salmon,SandyBrown,SeaGreen,SeaShell,Sienna,Silver,SkyBlue,SlateBlue,SlateGrey,Snow,SpringGreen,SteelBlue,Tan,Teal,Thistle,Tomato,Turquoise,Violet,Wheat,White,WhiteSmoke,Yellow,YellowGreen";
+
+inquirer
+    .prompt(questions)
+    .then( (answers) => {
+        const { text, textColor, shape, shapeColor } = answers;
+
+
+    })
