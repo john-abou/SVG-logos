a6aea0d180b102fd4e298e5116861825bff63fcc
John Abou <abounassar.john@gmail.com>
2023-02-12 20:54:27 -0500

    Added methods to the shape class to update the text, colorText and shapeColor

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -1,12 +1,15 @@
 // Must have parent Class called shape -- common traits include text, text color, shape color, height, width
 const fs = require('fs');
 
+const hexRegex = /^([a-fA-F0-9]{6}||[a-fA-F0-9]{3})$/;
+const colorKeywords = "AliceBlue,AntiqueWhite,Aqua,Aquamarine,Azure,Beige,Bisque,Black,BlanchedAlmond,Blue,BlueViolet,Brown,BurlyWood,CadetBlue,Chartreuse,Chocolate,Coral,CornflowerBlue,Cornsilk,Crimson,Cyan,DarkBlue,DarkCyan,DarkGoldenRod,DarkGrey,DarkGreen,DarkKhaki,DarkMagenta,DarkOliveGreen,Darkorange,DarkOrchid,DarkRed,DarkSalmon,DarkSeaGreen,DarkSlateBlue,DarkSlateGrey,DarkTurquoise,DarkViolet,DeepPink,DeepSkyBlue,DimGray,DodgerBlue,FireBrick,FloralWhite,ForestGreen,Fuchsia,Gainsboro,GhostWhite,Gold,GoldenRod,Grey,Green,GreenYellow,HoneyDew,HotPink,IndianRed,Indigo,Ivory,Khaki,Lavender,LavenderBlush,LawnGreen,LemonChiffon,LightBlue,LightCoral,LightCyan,LightGoldenRodYellow,LightGrey,LightGreen,LightPink,LightSalmon,LightSeaGreen,LightSkyBlue,LightSlateGrey,LightSteelBlue,LightYellow,Lime,LimeGreen,Linen,Magenta,Maroon,MediumAquaMarine,MediumBlue,MediumOrchid,MediumPurple,MediumSeaGreen,MediumSlateBlue,MediumSpringGreen,MediumTurquoise,MediumVioletRed,MidnightBlue,MintCream,MistyRose,Moccasin,NavajoWhite,Navy,OldLace,Olive,OliveDrab,Orange,OrangeRed,Orchid,PaleGoldenRod,PaleGreen,PaleTurquoise,PaleVioletRed,PapayaWhip,PeachPuff,Peru,Pink,Plum,PowderBlue,Purple,Red,RosyBrown,RoyalBlue,SaddleBrown,Salmon,SandyBrown,SeaGreen,SeaShell,Sienna,Silver,SkyBlue,SlateBlue,SlateGrey,Snow,SpringGreen,SteelBlue,Tan,Teal,Thistle,Tomato,Turquoise,Violet,Wheat,White,WhiteSmoke,Yellow,YellowGreen";
+
 class Shape {
     constructor(text="SVG", textColor="white", shape="", shapeColor="black") {
-        this.text = text;
-        this.textColor = textColor;
+        this.text = textCheck(text);
+        this.textColor = colorCheck(textColor);
         this.shape = shape;
-        this.shapeColor = shapeColor;
+        this.shapeColor = colorCheck(shapeColor);
     }
 
     render() {
@@ -35,36 +38,48 @@ class Shape {
     `
         fs.writeFile('square.html', svgHTML, (err) => err ? console.log('Square SVG generated') : console.error(err))
     }
+
+    updateTextColor( updatedColor ) {
+        this.textColor = colorCheck(updatedColor);
+        console.log("Rerun the render method to see the changes!")
+    }
+
+    updateText( updatedText ) {
+        this.text = textCheck(updatedText);
+        console.log("Rerun the render method to see the changes!")
+    }
+
+    updateShapeColor( updatedColor ) {
+        this.shapeColor = colorCheck(updatedColor);
+        console.log("Rerun the render method to see the changes!")
+    }
 }
 
+// Create all the child classes of the shape class
 class Square extends Shape {
     constructor(text, textColor, shape, shapeColor) {
         super(text, textColor, shapeColor);
         this.shape = 'rect x="55" y="10" width="190" height="190"';
     }
 }
-
 class Rectangle extends Shape {
     constructor(text, textColor, shape, shapeColor) {
         super(text, textColor, shapeColor);
         this.shape = 'rect x="25" y="25" width="270" height="170"';
     }
 }
-
 class Circle extends Shape {
     constructor(text, textColor, shape, shapeColor) {
         super(text, textColor, shapeColor);
         this.shape = 'circle cx="150" cy="100" r="90" fill="red"';
     }
 }
-
 class Triangle extends Shape {
     constructor(text, textColor, shape, shapeColor) {
         super(text, textColor, shapeColor);
         this.shape = 'polygon points="150,10 290,190 10,190"';
     }
 }
-
 class Ellipse extends Shape {
     constructor(text, textColor, shape, shapeColor) {
         super(text, textColor, shapeColor);
@@ -72,22 +87,23 @@ class Ellipse extends Shape {
     }
 }
 
+// Create a function to test that the text is 3 letters
+function textCheck( textInput ) {
+    if (textInput.length > 3) {
+      throw new Error('Your input is greater than 3 characters. Try again.')
+    } else {
+      return textInput;
+    }
+}
+  
+// Create a function to test that the color entered is a hexcode or color
+function colorCheck( color ) {
+    if (hexRegex.test(color) || colorKeywords.includes(color.toLowerCase())) {
+        return color
+    } else {
+        throw new Error('The color entered is not a hex string or html color. Try again.')
+    }
+}
 
-module.exports = {Shape, Square}
-
-/*
-    html = `<svg xmlns="http://www.w3.org/2000/svg" height="200" width="300" style="text-align: center;">
-    <!-- rectangle -->
-    <ellipse cx="150" cy="100" rx="125" ry="75" style="fill:${shapeColor};stroke:purple;stroke-width:2" />
-
-    <text x="50%" y="50%" style="font-size: 32px;  text-align:center;" text-anchor="middle" alignment-baseline="middle" fill=${textColor}>
-    ${text}
-    </text>
-    </svg>`;
-
-    <rect x="25" y="25" width="270" height="170" fill="black"/> 
-    <rect x="55" y="10" width="190" height="190" fill="black"/> 
-    <circle cx="150" cy="100" r="90" fill="red" stroke="black" stroke-width="1" />
-    <polygon points="150,10 290,190 10,190" style="fill:lime;stroke:purple;stroke-width:1" />
-    <ellipse cx="150" cy="100" rx="125" ry="75" style="fill:yellow;stroke:purple;stroke-width:2" />
-*/
+// Export the classes and functions used in the classes
+module.exports = {Square, Rectangle, Circle, Triangle, Ellipse, textCheck, colorCheck}
--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-/* 
-GIVEN a command-line application that accepts user input
-
-WHEN I am prompted for text THEN I can enter up to three characters.
-WHEN I am prompted for the text color THEN I can enter a color keyword (OR a hexadecimal number)
-WHEN I am prompted for a shape THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-WHEN I am prompted for the shape's color THEN I can enter a color keyword (OR a hexadecimal number)
-WHEN I have entered input for all the prompts THEN an SVG file is created named `logo.svg`AND the output text "Generated logo.svg" is printed in the command line
-WHEN I open the `logo.svg` file in a browser THEN I am shown a 300x200 pixel image that matches the criteria I entered 
-
-*/
-
+/*
 // Write a prompt to accept 3 letters, text color (keyword or hexadecimal), list of shapes, shape color (keyword or hexa).
 
 // Based on input from the prompt, SVG  file is created with named 'logo.svg'. Console log "Genererated logo.svg".
@@ -21,11 +10,12 @@ WHEN I open the `logo.svg` file in a browser THEN I am shown a 300x200 pixel ima
 // From input instantiate a new class of the selected shape
 // Use classes methods to update text, text color, shape color --> these methods should be in Shape class (parent)
 // Use child class to have a specific method to make shape height, width, etc 
+*/
 
 // Import dependencies and Shape objects
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Shape, Square} = require('./Develop/lib/shapes.js');
+const { Square, Rectangle, Circle, Triangle, Ellipse, textCheck, colorCheck} = require('./Develop/lib/shapes.js');
 
 // Define global variables
 const questions = [
@@ -57,41 +47,25 @@ const questions = [
       message: 'Enter a shape color:'
     }
 ];
-const hexRegex = /^([a-fA-F0-9]{6}||[a-fA-F0-9]{3})$/;
-const colorKeywords = "AliceBlue,AntiqueWhite,Aqua,Aquamarine,Azure,Beige,Bisque,Black,BlanchedAlmond,Blue,BlueViolet,Brown,BurlyWood,CadetBlue,Chartreuse,Chocolate,Coral,CornflowerBlue,Cornsilk,Crimson,Cyan,DarkBlue,DarkCyan,DarkGoldenRod,DarkGrey,DarkGreen,DarkKhaki,DarkMagenta,DarkOliveGreen,Darkorange,DarkOrchid,DarkRed,DarkSalmon,DarkSeaGreen,DarkSlateBlue,DarkSlateGrey,DarkTurquoise,DarkViolet,DeepPink,DeepSkyBlue,DimGray,DodgerBlue,FireBrick,FloralWhite,ForestGreen,Fuchsia,Gainsboro,GhostWhite,Gold,GoldenRod,Grey,Green,GreenYellow,HoneyDew,HotPink,IndianRed,Indigo,Ivory,Khaki,Lavender,LavenderBlush,LawnGreen,LemonChiffon,LightBlue,LightCoral,LightCyan,LightGoldenRodYellow,LightGrey,LightGreen,LightPink,LightSalmon,LightSeaGreen,LightSkyBlue,LightSlateGrey,LightSteelBlue,LightYellow,Lime,LimeGreen,Linen,Magenta,Maroon,MediumAquaMarine,MediumBlue,MediumOrchid,MediumPurple,MediumSeaGreen,MediumSlateBlue,MediumSpringGreen,MediumTurquoise,MediumVioletRed,MidnightBlue,MintCream,MistyRose,Moccasin,NavajoWhite,Navy,OldLace,Olive,OliveDrab,Orange,OrangeRed,Orchid,PaleGoldenRod,PaleGreen,PaleTurquoise,PaleVioletRed,PapayaWhip,PeachPuff,Peru,Pink,Plum,PowderBlue,Purple,Red,RosyBrown,RoyalBlue,SaddleBrown,Salmon,SandyBrown,SeaGreen,SeaShell,Sienna,Silver,SkyBlue,SlateBlue,SlateGrey,Snow,SpringGreen,SteelBlue,Tan,Teal,Thistle,Tomato,Turquoise,Violet,Wheat,White,WhiteSmoke,Yellow,YellowGreen";
  
-
+/*
 inquirer
     .prompt(questions)
     .then( (answers) => {
       const { text, textColor, shape, shapeColor } = answers;
       
+      // Create different cases for whatever the user choose to create
+      switch ( shape ) {
+        case value:
+          
+          break;
       
+        default:
+          break;
+      }      
 
     })
+*/
 
-
-// Create a function to test that the text is 3 letters
-function textCheck( textInput ) {
-  if (textInput.length > 3) {
-    textInput = "SVG"
-    return textInput;
-  } else {
-    return textInput;
-  }
-}
-
-// Create a function to test that the color entered is a hexcode or color
-function colorCheck( color ) {
-  if (hexRegex.includes(color) || colorKeywords.includes(color)) {
-    return color
-  } else {
-    color = "black";
-    return color;
-  }
-}
-
-/*
 let testShape = new Square("SVG", "white", "square", "black");
 testShape.render()
-*/
